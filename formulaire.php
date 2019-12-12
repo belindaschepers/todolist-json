fc45e9e38deb5231aae9fe24f59912b73654bfbe
Belinda Schepers <belindaschepers1@gmail.com>
2019-12-12 16:31:17 +0100

    updates

--- formulaire.php
+++ formulaire.php
@@ -1,24 +1,38 @@
-<div class="formu">
-    <form method="post" action="index.php" class="add">
-        <p>Ajouter une tâche</p>
-        <div class="add-items d-flex"> 
-            <input type="text-area" name="task" class="form-control todo-list-input" placeholder="Que devez-vous faire?"> 
-            <button class="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> 
-        </div>
-    </form>
-    
-    </div>
-
 <?php
-if(isset($_POST['submit'])){
-
-    $san = filter_var ($_POST['task'], FILTER_SANITIZE_STRING);
-    $data = file_get_contents('todo.json');
-    $jsonArray = json_decode($data, true);//décoder le json contenu dans ma variable
-
-    $jsonArray[] = array("taches" => $san, //ajouter les données récoltées 
-                        "valeurs" => true);
-
-    file_put_contents('todo.json', json_encode($jsonArray));//enregistrer les modifs
-}
+    $myTasks = [];
+    $stat=false;
+    $newTask = $_POST['tache']; 
+    $checked = $_POST['checked'];
+    $content = file_get_contents("todo.json"); 
+    $phpContent = json_decode($content); 
+    if($newTask != "") {
+        foreach ($phpContent as $value) {
+            if($value->tache==$newTask){
+                $value->checked=$checked;
+            }
+            else{
+                array_push($myTasks, $value); // Ajout des tâches dans un tableau
+            }
+        };
+    
+        if ($newTask != "") {
+            $newTask = filter_var($newTask, FILTER_SANITIZE_STRING); 
+        } else {
+            $newTask = null;
+        }
+    
+        $task =  array(
+            'tache' => $newTask,
+            'checked' => ($checked == "true" ? true :false)
+        ); // Créer la nouvelle tache dans un tableau
+    
+        array_push($myTasks, $task); // Push de la nouvelle tache dans le tableau
+    
+        $json = json_encode($myTasks); 
+        if(file_put_contents("todo.json", $json)) //Save
+            echo 'success';
+        else
+            echo 'failed';
+        
+    }
 ?>
--- index.php
+++ index.php
@@ -1,42 +1,78 @@
 <?php
-    if(!empty($_POST['check'])){
-    for ($a = 0; $a <count($jsonArray); $a++){ 
-        echo "bonjour";
-            $value['valeurs'] = false;
-    };
-    }
+include ('formulaire.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=lg, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todo List En Ajax</title>
-    <link rel="stylesheet" href="style.css">
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
 
-<div class="page-content page-container" id="page-content">
-    <div class="padding">
-        <div class="row container d-flex justify-content-center">
-            <div class="col-lg-12">
-                <div class="card px-3">
-                    <div class="card-body">
-                        <h1 class="card-title">Todo list</h1>
-                        <?php 
-                         include("formulaire.php");?>
-                          <div class="container row">
-                          <?php    
-                          include("a-faire.php");
-                          ?>
-                        </div>
+    <!--CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!--Font-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <title>Todo List Json</title>
+
+  </head>
+
+  <body>
+    <div class="row justify-content-center mt-2">
+        <h2>To Do List</h2>
+    </div>
+    
+    <div class="row justify-content-center mt-2">
+        <img src="Images/robot-1470108_960_720.png" alt="robotList">
+    </div>
+
+    <div class="container">
+        <form method="post" action="index.php" class="add" id="form">
+       
+            <div class="row mt-5">
+                <input type="text-area" class="form-control" id="tache" placeholder="Ajouter une tâche">
+            </div> 
         
+            </div>
+            <div class="row justify-content-center mt-3">
+                <input type="submit" class="btn btn-danger" value="Enregister" name="submit" />
+            </div>           
+        </form>
+    </div>
+    <div class="text-center mt-5">
+    <h3>Tâches</h3>
+    </div>
+    <div class="text-center mt-5 afaire" id="todo">
+        
+    </div>
+      
+    <div class="row justify-content-center mt-3">
+        <input type="submit" class="btn btn-danger" value="Archiver" name="submit" id="save" />
+    </div>
+    <div class="row justify-content-center mt-5">
+        <h3>Archives</h3> 
+    </div>
+    <div  class="text-center archive" id="target">
 
-</body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>$( function() {
+    $( "#todo" ).sortable();
+    $( "#todo" ).disableSelection();
+  } );</script>
+ </body>
 </html>
